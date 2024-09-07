6dd9e9b23f19cbbba4acfd33779ab0d97774a1f4
RodrigoRamos-DEV <rodrigomramos18@gmail.com>
2024-09-07 08:33:54 -0300

    v1.3

--- src/Componentes/NavBar/index.jsx
+++ src/Componentes/NavBar/index.jsx
@@ -4,22 +4,23 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './styles.css';
+import Logo from '../../Img/logo.png';
 
 
 
 function NavScrollExample() {
    
   return (
+    
+      
+    
     <Navbar expand="lg" className="bg-body-tertiary">
+     
       <Container fluid>
         
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
+     
+      
+            
             <Nav.Link id="menu" href="/home"> Home</Nav.Link>
             <Nav.Link id="menu"  href="/projeto">Projeto</Nav.Link>
             <Nav.Link id="menu"  href="#action2">Apoiadores</Nav.Link>
@@ -28,9 +29,8 @@ function NavScrollExample() {
           
               
            
-          </Nav>
           
-        </Navbar.Collapse>
+       
       </Container>
     </Navbar>
     
