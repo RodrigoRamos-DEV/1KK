da085138b512ca07e7ffeb467078c0d0d8d56837
RodrigoRamos-DEV <rodrigomramos18@gmail.com>
2024-09-09 15:06:54 -0300

    v1.4 limpeza

--- src/Componentes/NavBar/index.jsx
+++ src/Componentes/NavBar/index.jsx
@@ -20,11 +20,11 @@ function NavScrollExample() {
         
      
       
-            
+            <img src={Logo} className='logo'/>
             <Nav.Link id="menu" href="/home"> Home</Nav.Link>
             <Nav.Link id="menu"  href="/projeto">Projeto</Nav.Link>
             <Nav.Link id="menu"  href="#action2">Apoiadores</Nav.Link>
-            <Nav.Link id="menu"  href="#action2">Compre</Nav.Link>
+            <Nav.Link id="menu"  href="https://pay.kirvano.com/1cf71384-a26b-4a35-bd37-df657f4f85af">Compre</Nav.Link>
             <Nav.Link id="menu"  href="https://www.instagram.com/apaginade1kk/?ref=badge">Contato</Nav.Link>
           
               
